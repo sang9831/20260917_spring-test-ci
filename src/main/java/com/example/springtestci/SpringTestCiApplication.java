e764f3bc60cece6d2e1259f5fcca7568c7f27e68
sangmin <tjfl9831@gmail.com>
2026-09-17 09:25:58 +0900

    Initial commit

---
+++ src/main/java/com/example/springtestci/SpringTestCiApplication.java
@@ -0,0 +1,13 @@
+package com.example.springtestci;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringTestCiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringTestCiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springtestci/SpringTestCiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springtestci;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringTestCiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
